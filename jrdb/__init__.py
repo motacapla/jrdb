a4f6915e528f7a2f1a3ca284d3bf8bd60daabdc4
motacapla <t.i.motacapla@gmail.com>
2023-07-13 19:59:10 +0900

    fix imports

--- jrdb/__init__.py
+++ jrdb/__init__.py
@@ -1,2 +1,2 @@
-from jrdb import load
-from jrdb import parse
+#from jrdb import load
+#from jrdb import parse
--- jrdb/parse.py
+++ jrdb/parse.py
@@ -2,7 +2,7 @@ import re
 import numpy as np
 import pandas as pd
 
-from jrdb import load
+from jrdb.jrdb import load
 
 class JrdbDataParser():
     def __init__(self):
